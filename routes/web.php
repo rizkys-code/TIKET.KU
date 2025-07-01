e89f5151a830dfefebd28f0ca4a07f176d424a1b
rizkys-code <2311500173@student.budiluhur.ac.id>
2025-07-01 21:57:36 +0700

    -

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,24 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\JadwalController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::redirect('/', '/auth/login')->name('home');
+Route::redirect('/', '/auth/login');
 
-Route::prefix('auth')->group(function () {
-
+Route::prefix('auth')->middleware('guest')->group(function () {
     Route::get('login', [LoginController::class, 'index'])->name('login');
-    Route::post('login', [LoginController::class, 'login'])->name('login.post');
+    Route::post('login', [LoginController::class, 'login']); 
 
     Route::get('register', [RegisterController::class, 'index'])->name('register');
-    Route::post('register', [RegisterController::class, 'register'])->name('register.post');
-
-    // Route::get('logout', [LoginController::class, 'logout'])->name('logout');
+    Route::post('register', [RegisterController::class, 'register']); 
 });
 
+
+
+Route::middleware('auth')->group(function () {
+    
+    Route::get('jadwal', [JadwalController::class, 'index'])->name('jadwal');
+    Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+});
